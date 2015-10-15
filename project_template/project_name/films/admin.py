d43235b796c6c55d69442dfe5d0b0c91ecd31fae
Enmanuel Martinez <enmanuel.martinez.g@gmail.com>
2015-10-15 16:54:10 -0400

    quit adminx and migrations from films, quit south configuration from settings

--- project_template/project_name/films/admin.py
+++ project_template/project_name/films/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Actor, Category, Film, Language
 
 # Register your models here.
+
+admin.site.register(Actor)
+admin.site.register(Category)
+admin.site.register(Film)
+admin.site.register(Language)
+
--- project_template/project_name/project_name/settings.py
+++ project_template/project_name/project_name/settings.py
@@ -210,7 +210,6 @@ DJANGO_APPS = (
 
 # Apps specific for this project go here.
 LOCAL_APPS = (
-    'xadmin',
     'crispy_forms',
     'reversion',
     'films',
@@ -260,17 +259,6 @@ LOGGING = {
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#wsgi-application
 WSGI_APPLICATION = '%s.wsgi.application' % SITE_NAME
 ########## END WSGI CONFIGURATION
-
-
-########## SOUTH CONFIGURATION
-# See: http://south.readthedocs.org/en/latest/installation.html#configuring-your-django-installation
-INSTALLED_APPS += (
-    # Database migration helpers:
-    'south',
-)
-# Don't need to use South when setting up a test database.
-SOUTH_TESTS_MIGRATE = False
-########## END SOUTH CONFIGURATION
 
 
 ########## TOOLBAR CONFIGURATION
